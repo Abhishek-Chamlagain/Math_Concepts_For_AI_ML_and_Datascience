189c04c92f17da1697355b2e7d350bf690ca89c4
Math_Concepts_For_AI_ML_and_Datascience <abishekchamlagain@gmail.com>
2026-01-02 22:44:40 +0545

    Add example

--- Probability.py
+++ Probability.py
@@ -2,3 +2,7 @@
 """It is a measure of likelihood or chances that an event will occur
 Probability lies between 0 and 1, so maximum wil be one (Always True), 
 and minimum wll be zero(Always False)"""
+
+# Example
+""" P(sun rises from east) = always True = 1
+    P(sun rises from west) = always False = 0"""
