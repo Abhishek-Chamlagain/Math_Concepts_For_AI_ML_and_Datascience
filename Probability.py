ca5b499afbcf968cc12e6e70b70e5dcbcc7a4ee2
Math_Concepts_For_AI_ML_and_Datascience <abishekchamlagain@gmail.com>
2026-01-02 20:29:58 +0545

    Add Probability file

---
+++ Probability.py
@@ -0,0 +1,4 @@
+# Probability 
+"""It is a measure of likelihood or chances that an event will occur
+Probability lies between 0 and 1, so maximum wil be one (Always True), 
+and minimum wll be zero(Always False)"""
